62f88b08bd10ad91e61b5fa981eac77064eb618b
Stefano Durì <dust.bo@gmail.com>
2026-02-06 13:23:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Assicurati che questo corrisponda al nome del repo: arvaia---distribuzione
-  base: '/arvaia---distribuzione/', 
+  // Assicurati che questo corrisponda al nome del repo: arvaia-distribuzione
+  base: '/arvaia-distribuzione/', 
 });
